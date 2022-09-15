9b03350b16f27b923f9424d25a9436f92659481a
JHON TEJADA DURANGO <jhontejada13@gmail.com>
2022-09-14 23:33:45 -0500

    Fix relaciones Turista Reservas Hotel

--- Migrations/20220915043221_Fix_Relacion_Turista_Reservas_Hotel.Designer.cs
+++ Migrations/20220915043221_Fix_Relacion_Turista_Reservas_Hotel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReservasAPI.Data;
 
@@ -11,9 +12,10 @@ using ReservasAPI.Data;
 namespace ReservasAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220915043221_Fix_Relacion_Turista_Reservas_Hotel")]
+    partial class Fix_Relacion_Turista_Reservas_Hotel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
